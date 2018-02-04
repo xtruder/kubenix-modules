feb41b1631a89db1a85b150292ea5ddf4d29bb92
Jaka Hudoklin <jakahudoklin@gmail.com>
2018-02-04 14:07:03 +0100

    fix(parity): downgrade to latest stable version

--- services/parity.nix
+++ services/parity.nix
@@ -9,7 +9,7 @@ with lib;
       image = mkOption {
         description = "Name of the parity image to use";
         type = types.str;
-        default = "parity/parity:v1.9.2";
+        default = "parity/parity:v1.8.9";
       };
 
       replicas = mkOption {
