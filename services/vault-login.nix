e745741849af9fce8c196dfe78ca89663c7adcef
Jaka Hudoklin <jakahudoklin@gmail.com>
2018-04-06 19:55:53 +0200

    fix(vault-login): set assignAsDefaults

--- services/vault-login.nix
+++ services/vault-login.nix
@@ -6,6 +6,7 @@ with k8s;
 {
   # kubenix module that implements vault login sidecar that 
   kubernetes.moduleDefinitions.vault-login-sidecar.prefixResources = false;
+  kubernetes.moduleDefinitions.vault-login-sidecar.assignAsDefaults = true;
   kubernetes.moduleDefinitions.vault-login-sidecar.module = { name, module, config, ... }: {
     options = {
       resourcePath = mkOption {
