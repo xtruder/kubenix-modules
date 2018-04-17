e892d3b3a0594f213b0f77269c240736008f5d61
Uros Hercog <uros.hercog@gatehub.net>
2018-04-17 11:23:48 +0200

    fix CA cert loding for vault ui

--- services/vault-ui.nix
+++ services/vault-ui.nix
@@ -49,7 +49,7 @@ with k8s;
                 env = {
                   VAULT_URL_DEFAULT.value = config.vault.defaultUrl;
                   VAULT_AUTH_DEFAULT.value = config.vault.defaultAuth;
-                  CUSTOM_CA_CERT.value = "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt";
+                  NODE_EXTRA_CA_CERTS.value = "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt";
                 };
                 resources = {
                   requests.memory = "50Mi";
