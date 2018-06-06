43d82b81b8b7a7dbcca4cd0791a21489c4277b10
Rok Černič <rok.cernic@gatehub.net>
2018-06-06 10:27:08 +0200

    fix(bitcoind): tests

--- test/bitcoind.nix
+++ test/bitcoind.nix
@@ -5,5 +5,8 @@
 
   kubernetes.modules.bitcoind = {
     module = "bitcoind";
+    configuration = {
+      rpcAuth = "test:a7d424b74122e17362e404ec5c5e6d$822c00a871d66c16b7a8ebcc7189624a74e4c2d46e31993012d1d36ade576363";
+    };
   };
 }
