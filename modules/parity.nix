07c3024e4cce4ae8ab93eef521bce018e2596b4f
Jaka Hudoklin <jaka@gatehub.net>
2018-11-07 15:07:47 +0100

    bump(parity): v2.0.9 -> v2.1.2

--- modules/parity.nix
+++ modules/parity.nix
@@ -9,7 +9,7 @@ with lib;
       image = mkOption {
         description = "Name of the parity image to use";
         type = types.str;
-        default = "parity/parity:v2.1.2";
+        default = "parity/parity:v2.0.9";
       };
 
       replicas = mkOption {
