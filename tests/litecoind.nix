1056fec977b68f29d7b4be31f670b56293d867a1
Rok Černič <rok.cernic@gmail.com>
2018-11-13 14:19:11 +0100

    feat(litecoind): add module

---
+++ tests/litecoind.nix
@@ -0,0 +1,12 @@
+{ config, ... }:
+
+{
+  require = [../modules/litecoind.nix];
+
+  kubernetes.modules.litecoind = {
+    module = "litecoind";
+    configuration = {
+      rpcAuth = "test:a7d424b74122e17362e404ec5c5e6d$822c00a871d66c16b7a8ebcc7189624a74e4c2d46e31993012d1d36ade576363";
+    };
+  };
+}
