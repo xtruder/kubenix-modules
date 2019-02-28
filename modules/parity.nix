0b2661527ce5c5fb16186e8e825df9b6669bebcd
Rok Černič <rok.cernic@gmail.com>
2019-02-28 12:56:29 +0100

    bump(parity): v2.2.9 -> v2.2.11

--- modules/parity.nix
+++ modules/parity.nix
@@ -9,7 +9,7 @@ with lib;
       image = mkOption {
         description = "Name of the parity image to use";
         type = types.str;
-        default = "parity/parity:v2.2.9";
+        default = "parity/parity:v2.2.11";
       };
 
       replicas = mkOption {
