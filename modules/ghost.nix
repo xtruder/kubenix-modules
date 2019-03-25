7bdb9679733a8544f80d4a45ab5be1da1785dad1
janz <janz@gatehub.net>
2019-03-25 13:54:02 +0100

    ghost config

---
+++ modules/ghost.nix
@@ -0,0 +1,151 @@
+{ config, lib, k8s, ... }:
+
+with lib;
+with k8s;
+
+{
+  config.kubernetes.moduleDefinitions.ghost.module = {module, config, ...}: {
+    options = {
+      image = mkOption {
+        description = "Docker image to use";
+        type = types.str;
+        default = "ghost:2-alpine";
+      };
+
+      replicas = mkOption {
+        description = "Number of ghost replicas to run";
+        type = types.int;
+        default = 1;
+      };
+
+      database = {
+        type = mkOption {
+          description = "Database type";
+          type = types.enum ["mysql"];
+          default = "mysql";
+        };
+
+        name = mkOption {
+          description = "Database name";
+          type = types.str;
+          default = "ghost";
+        };
+
+        host = mkOption {
+          description = "Database host";
+          type = types.str;
+          default = "mysql";
+        };
+
+        port = mkOption {
+          description = "Database port";
+          type = types.int;
+          default = 3306;
+        };
+
+        username = mkSecretOption {
+          description = "Database user";
+          default.key = "username";
+        };
+
+        password = mkSecretOption {
+          description = "Database password";
+          default.key = "password";
+        };
+      };
+
+    #   mongodbUri = mkOption {
+    #     description = "URI for mongodb database";
+    #     type = types.str;
+    #     default = "mongodb://mongo/ghost";
+    #   };
+
+      extraPorts = mkOption {
+        description = "Extra ports to expose";
+        type = types.listOf types.int;
+        default = [];
+      };
+    };
+
+    config = {
+      kubernetes.resources.statefulSets.ghost = {
+        metadata = {
+          name = module.name;
+          labels.app = module.name;
+        };
+        spec = {
+          serviceName = module.name;
+          replicas = config.replicas;
+          selector.matchLabels.app = module.name;
+          template = {
+            metadata.labels.app = module.name;
+            spec = {
+              affinity.podAntiAffinity.preferredDuringSchedulingIgnoredDuringExecution = [{
+                weight = 100;
+                podAffinityTerm = {
+                  labelSelector.matchExpressions = [{
+                    key = "app";
+                    operator = "In";
+                    values = [ module.name ];
+                  }];
+                  topologyKey = "kubernetes.io/hostname";
+                };
+              }];
+
+              containers.ghost = {
+                image = config.image;
+                env.database__client.value = confif.database.type;
+                env.database__connection__database.value = config.database.name;
+                env.database__connection__password = secretToEnv config.database.password;
+                env.database__connection__host.value = config.database.host;
+                env.database__connection__user.value = secretToEnv config.database.username;
+                
+
+                securityContext.capabilities.add = ["NET_ADMIN"];
+
+                ports = [{
+                  containerPort = 2368;
+                } ] ++ map (port: {containerPort = port;}) config.extraPorts;
+                volumeMounts = [{
+                  name = "data";
+                  mountPath = "/ghost/data";
+                }];
+              };
+            };
+          };
+          volumeClaimTemplates = [{
+            metadata.name = "data";
+            spec = {
+              accessModes = ["ReadWriteOnce"];
+              resources.requests.storage = "1G";
+            };
+          }];
+        };
+      };
+
+      kubernetes.resources.podDisruptionBudgets.ghost = {
+        metadata.name = module.name;
+        metadata.labels.app = module.name;
+        spec.minAvailable = 1;
+        spec.selector.matchLabels.app = module.name;
+      };
+
+      kubernetes.resources.services.ghost = {
+        metadata.name = module.name;
+        metadata.labels.app = module.name;
+
+        spec.selector.app = module.name;
+
+        spec.ports = [{
+          name = "http";
+          port = 80;
+          targetPort = 2368;
+        }] ++ map (port: {
+          name = "${toString port}";
+          port = port;
+          targetPort = port;
+        }) config.extraPorts;
+      };
+    };
+  };
+}
